f967e3d7916818e2c18895095897e99469012da3
zxey <zxey@users.noreply.github.com>
2017-07-10 17:07:52 +0200

    Remove debug code

--- game/main.lua
+++ game/main.lua
@@ -13,78 +13,8 @@ function love.load()
     love.graphics.setFont(love.graphics.newFont("fonts/OpenSans-Light.ttf", 18))
 
     love.window.setMode(800, 600, {x=1119, y=25, resizable=true})
-    -- love.window.setMode(800, 600, {x=2100, y=800, resizable=true})
     game = Game()
-
-    -- view = ui.column {
-    --     id="rootcolumn",
-    --     x=0,y=0,spacing=20,
-    --     ui.repeater {
-    --         id="toprepeater",
-    --         times=function(self) return count end,
-    --         ui.button {
-    --             id="toprepeaterbutton",
-    --             radius=20,
-    --             onPressed=function(self) return function() count = count + 1 end end
-    --         },
-    --     },
-    --     ui.button {
-    --         id="increment",
-    --         onPressed=function(self) return function() count = count + 1 end end
-    --     },
-    --     ui.button {
-    --         id="decrement",
-    --         onPressed=function(self) return function() count = count - 1 end end
-    --     },
-    --     ui.repeater {
-    --         id="bottomrepeater",
-    --         times=function(self) return count end,
-    --         ui.row {
-    --             id="bottomrepeaterrow",
-    --             ui.repeater {
-    --                 id="rowbutton",
-    --                 times=3,
-    --                 ui.button {
-    --                     radius=20,
-    --                     onPressed=function(self) return function() self.parent.times = self.parent.times + 1 end end
-    --                 },
-    --             }
-    --         }
-    --     },
-    -- }
 end
-
--- function love.update(dt)
---     view:update()
---     --print(inspect(view))
--- end
-
--- function love.draw()
---     view:draw()
--- end
-
--- function love.mousepressed(x, y, button, istouch)
---     local pressed = view:mousePressed(x, y, button, istouch)
---     print("mousepressed", x, y, button, pressed)
-
--- end
-
--- function love.mousereleased(x, y, button, istouch)
---     local released = view:mouseReleased(x, y, button, istouch)
---     print("mousereleased", x, y, button, released)
--- end
-
--- function love.mousemoved(x, y, dx, dy, istouch)
---     local moved = view:mouseMoved(x, y, dx, dy, istouch)
---     -- print("mousemoved", x, y, dx, dy, moved)
--- end
-
--- function love.keypressed(key)
---    if key == "tab" then
---       local state = not love.mouse.isGrabbed()   -- the opposite of whatever it currently is
---       love.mouse.setGrabbed(state)
---    end
--- end
 
 function love.update(dt)
     game:update(dt)
@@ -92,7 +22,6 @@ end
 
 function love.draw()
     game:draw()
-    love.graphics.print(displayText)
 end
 
 function love.keypressed(key, scancode, isrepeat)
@@ -116,7 +45,6 @@ function love.mousemoved(x, y, dx, dy, istouch)
 end
 
 function love.textinput(text)
-    displayText = displayText .. text
     game:textInput(text)
 end
 
